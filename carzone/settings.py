0719a600b2da240eb88dc3c8429cb15d4e50b1c5
ASPeXP <prapaths@hotmail.com>
2023-09-01 03:05:23 +0700

    update allowed host for test

--- carzone/settings.py
+++ carzone/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 LOGIN_REDIRECT_URL = 'dashboard'
 
 # Application definition
